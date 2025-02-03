9ed9c434057262fb8df70c9f99f41c693d35938c
Dusousas <dusousadeveloper@gmail.com>
2025-02-03 12:22:45 -0300

    testes

--- components/Missa.tsx
+++ components/Missa.tsx
@@ -18,7 +18,7 @@ export default function Missa() {
             <div className="relative w-full max-w-[600px] h-[500px] lg:h-[722px]">
               <Image
                 className="rounded-br-[20%] object-cover"
-                src="/missa3.jpg"
+                src="/about1.jpg"
                 alt="Imagem da missa"
                 fill
                 sizes="(max-width: 1024px) 90vw, 50vw"
@@ -27,7 +27,7 @@ export default function Missa() {
 
             {/* Coluna com imagens menores */}
             <div className="hidden lg:flex lg:flex-col lg:gap-4">
-              {['missa1.jpg', 'missa2.jpg', 'about1.jpg'].map((img, index) => (
+              {['about1.jpg', 'about1.jpg', 'about1.jpg'].map((img, index) => (
                 <div key={index} className="relative w-[450px] h-[230px]">
                   <Image
                     className="rounded-br-[20%] object-cover"
